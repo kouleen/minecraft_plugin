6a25be82fbbce0b94156876c5afdab6810f863b7
张青 <kouleen@aliyun.com>
2023-02-06 23:53:17 +0800

    - supperMessage success -v3

--- respawn/src/main/java/com/kouleen/respawn/ReSpawn.java
+++ respawn/src/main/java/com/kouleen/respawn/ReSpawn.java
@@ -8,8 +8,6 @@ import com.kouleen.respawn.service.ReSpawnService;
 import com.kouleen.respawn.service.ReSpawnServiceImpl;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
-
 public final class ReSpawn extends JavaPlugin {
 
     private final JavaPluginBean javaPluginBean = new JavaPluginBean();
@@ -25,8 +23,6 @@ public final class ReSpawn extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Logger logger = this.getLogger();
-        logger.info(String.format("%s 被成功加载了",getName()));
         ReSpawnService reSpawnService = javaPluginBean.getReSpawnService();
         reSpawnService.saveConfigFile(javaPluginBean);
         reSpawnService.registerEvents(javaPluginBean);
@@ -36,8 +32,6 @@ public final class ReSpawn extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        Logger logger = this.getLogger();
-        logger.info(String.format("%s 被成功卸载了",getName()));
         ReSpawnService reSpawnService = javaPluginBean.getReSpawnService();
         reSpawnService.saveConfigFile(javaPluginBean);
         super.onDisable();
